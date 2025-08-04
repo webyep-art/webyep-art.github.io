1c692fa5ec6006c20de58f7d9421d45465fdb22e
webyep <agofemboy@gmail.com>
2025-08-04 07:26:13 +0300

    Add files via upload

---
+++ header.js
@@ -0,0 +1,26 @@
+// header.js
+document.addEventListener('DOMContentLoaded', function() {
+    const headerHTML = `
+        <div id="header">
+            <a href="index.html" class="text_button">webyep</a>
+            <span class="separator">|</span>
+            <div class="dropdown">
+                <a href="#" class="text_button">projects</a>
+                <div class="dropdown-content">
+                    <a href="luna.html">Luna</a>
+                    <a href="pygame3d.html">Pygame 3D</a>
+                    <a href="sorty.html">Sorty</a>
+                </div>
+            </div>
+        </div>
+    `;
+    
+
+    document.body.insertAdjacentHTML('afterbegin', headerHTML);
+    
+   
+    const oldHeader = document.getElementById('header');
+    if (oldHeader && oldHeader !== document.getElementById('header')) {
+        oldHeader.remove();
+    }
+});
