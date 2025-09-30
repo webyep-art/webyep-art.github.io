c44d364708e4e3250b47546685e474d7b524a47d
webyep <agofemboy@gmail.com>
2025-10-01 00:38:35 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyCQBwQjFXDVZr-402sKr0H8RYYpcL0ROYs",
+    authDomain: "tetrasscii.firebaseapp.com",
+    projectId: "tetrasscii",
+    storageBucket: "tetrasscii.firebasestorage.app",
+    messagingSenderId: "387850136129",
+    appId: "1:387850136129:web:cc77d0319c479058e3f4c0",
+    measurementId: "G-PKE7BQXEDW"
+};
